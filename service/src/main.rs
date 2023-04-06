421bd0721438849f1508c35ba12c31bfb0b74ec6
Christian <chsu79@gmail.com>
2023-04-06 23:53:44 +0200

    post endpoint

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,11 +1,24 @@
 #[macro_use] extern crate rocket;
 
+use rocket::serde::{Deserialize, json::Json, json::json};
+
+#[derive(Deserialize)]
+struct Rumor {
+    rumor: String
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!\n"
 }
 
+#[post("/rumor", format = "json", data="<rumor>")]
+fn store(rumor : Json<Rumor>) -> rocket::serde::json::Value {
+  json!({"status": "success", "message": "please come again"})
+} 
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+      .mount("/", routes![index, store])
 }
